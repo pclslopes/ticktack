4759a9b374984664208004e72b15a67df0fdfc92
pclslopes <pclslopes@gmail.com>
2019-09-17 17:35:31 +0100

    2 files modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { LoginComponent } from './components/login/login/login.component';
+import { HomrComponent } from './components/home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    NbThemeModule.forRoot(),
+    NbLayoutModule,
+    NbEvaIconsModule
+  ],
+  declarations: [ AppComponent, HelloComponent, LoginComponent, HomrComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
